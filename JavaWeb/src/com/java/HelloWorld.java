242a2186105e258f1ceb852d06a846fc83678204
Mickey <1807121439@qq.com>
2020-02-12 20:04:17 +0800

    测试

--- JavaWeb/src/com/java/HelloWorld.java
+++ JavaWeb/src/com/java/HelloWorld.java
@@ -3,8 +3,9 @@ package com.java;
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("hello world");
-        System.out.println("你好！");
+        int i1 = Math.round(3849 / 833);
+        System.out.println(i1);
+
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("hello world");
-
+        int i1 = Math.round(3849 / 833);
+        System.out.println(i1);
     }
 }

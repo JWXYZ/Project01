1614d4cf737b3201c98a2c36e14d60eb672468a7
Mickey <1807121439@qq.com>
2020-02-12 20:58:12 +0800

    测试

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         int i1 = Math.round(3849 / 833);
         System.out.println(i1);
+
     }
 }
---
+++ src/com/company/QueryMoneyCount.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class QueryMoneyCount {
+    
+    public static void main(String[] args){
+                
+                
+    }
+    
+    
+}
